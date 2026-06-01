59e23dff8d32767d77575a7003ecf075484cd310
Fernanda <emanuela.fq@gmail.com>
2026-06-01 12:03:08 -0300

    Requerimiento 6

--- lisp/core.lisp
+++ lisp/core.lisp
@@ -1,1 +1,24 @@
-;Aca va el código de los requerimientos 1 al 6 
+;; ========================================================
+;; FUNCIÓN: porcentaje
+;; NATURALEZA: Pura
+;; ESTRATEGIA: Calculo aritmetico 
+;; IMPACTO: No destructiva
+;; ========================================================
+
+(defun porcentaje(tiempo-color total-ciclo) ; Total del ciclo rojo 90 + amarillo 6 + verde 120 = 216 seg
+    (float(* (/ tiempo-color total-ciclo) 100))
+)
+
+;; ========================================================
+;; FUNCIÓN: informe-distribucion-temporal
+;; NATURALEZA: Pura
+;; ESTRATEGIA: Composicion funcional
+;; IMPACTO: No destructiva
+;; ========================================================
+(defun informe-distribucion-temporal()
+    (list 'rojo (porcentaje 90 216)
+            'amarillo (porcentaje 6 216)
+            'verde (porcentaje 120 216)
+        )
+)
+
