c29eb1eea8b8407c7c1f5d1e93925005e3099a9d
Fabri <fago20000@gmail.com>
2026-06-01 19:22:39 -0300

    Funcion transicion con verificaciones correspondientes

--- lisp/core.lisp
+++ lisp/core.lisp
@@ -1,1 +1,19 @@
-;Aca va el código de los requerimientos 1 al 6 
+;;======================================== 
+;; REQUERIMIENTO 1: Estados de Transición 
+;; NATURALEZA: Pura 
+;; ESTRATEGIA: Evaluación de Patrones / Condicional Estricto 
+;; IMPACTO EN MEMORIA: No destructiva (Retorna una lista nueva) 
+;;========================================
+
+(defun transicion(color-actual cambiar-a) ;; Cambiar y hacer la validacion del ciclo Verde-amarillo amarillo-rojo  rojo-verde (hacer validacion inversa)
+    (let ((colores_incluidos (list 'en-rojo 'en-amarillo 'en-verde))(colores_cambio (list 'rojo 'amarillo 'verde)))
+        (cond 
+            ((not (member color-actual colores_incluidos)) nil) ;;Agregamos funcion para verificar que el color-actual este en los permitidos
+            ((and (eql 'en-verde color-actual)(eql 'amarillo cambiar-a)(list color-actual 'cambiar-a-amarillo)))
+            ((and (eql 'en-amarillo color-actual)(eql 'rojo cambiar-a)(list color-actual 'cambiar-a-rojo)))
+            ((and (eql 'en-rojo color-actual)(eql 'verde cambiar-a)(list color-actual 'cambiar-a-verde))) ;;La funcion member devolveria NIL en caso de que no se encuentre en la lista definida
+            (t (list color-actual 'accion-por-defecto)) ;; Ver donde se verifica el dato si aca o en alguna entrada
+                                                        ;; primer error no verificar el dato antes de devolver color-actual
+        )
+    )
+)
