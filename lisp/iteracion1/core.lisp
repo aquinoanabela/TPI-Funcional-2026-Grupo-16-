926263a40ed443c9feaa0d5afdf307c604565a86
Juan Cruz Galarza <juancruzgala@gmail.com>
2026-06-12 15:46:32 -0300

    Subo Requerimientos 4 al 6

--- lisp/iteracion1/core.lisp
+++ lisp/iteracion1/core.lisp
@@ -60,3 +60,75 @@
 )
 
 ;; (- (get-universal-time) 2208988800) que mide los segundos desde el 1 de enero de 1900  para calcular el tiempo Unix se le resta ese numero fijo de segundos
+
+;; REQUERIMIENTO 4: Análisis de Ciclos Semafóricos
+
+;; ========================================
+;; FUNCION: Duracion del ciclo
+;; NATURALEZA: Pura
+;; ESTRATEGIA: Calculo aritmetico
+;; IMPACTO EN MEMORIA: No destructiva
+;; ========================================
+
+(defun duracion-ciclo (&optional (seg-rojo 90)(seg-amarillo 6)(seg-verde 120));; &optional utiliza valores por defecto cuando llamas a la funcion sin pasarle parametros
+    (+ seg-rojo seg-amarillo seg-verde)                                       ;; pero dando la opcion a pasar parametros sin generar errores
+)
+
+;; ========================================
+;; FUNCION: Recomendacion del ciclo
+;; NATURALEZA: Pura
+;; ESTRATEGIA: Condicional (comparacion de rango s)
+;; IMPACTO EN MEMORIA: No destructiva
+;; ========================================
+
+(defun recomendacion-ciclo (duracion)
+  (cond
+    ((< duracion 0) 'duracion-invalida) ; por si es negativo
+    ((< duracion 35) 'ciclo-demasiado-corto)
+    ((and (>= duracion 35) (<= duracion 150)) 'ciclo-optimo)
+    ((> duracion 150) 'ciclo-demasiado-largo)
+  )
+) ;ver si la manera que muestro el msj esta bien
+
+;; REQUERIMIENTO 5: Planificación Temporal
+;; ========================================
+;; FUNCION: Ciclos por tiempo
+;; NATURALEZA: Pura
+;; ESTRATEGIA: Composicion funcional, calculo aritmetico y validación condicional
+;; IMPACTO EN MEMORIA: No destructiva
+;; ========================================
+
+(defun ciclos-por-tiempo (minutos) 
+   (let ((seg-rojo 90)
+         (seg-amarillo 6)
+         (seg-verde 120))
+
+      (if (< minutos 0) 'valor-invalido
+          (nth-value 0(truncate (* minutos 60)(duracion-ciclo seg-rojo seg-amarillo seg-verde))) ; con nth-value 0 me quedo con el primer valor 
+      )
+   )
+)
+;; REQUERIMIENTO 6: Informe de Distribución Temporal
+;; ========================================================
+;; FUNCIÓN: Porcentaje
+;; NATURALEZA: Pura
+;; ESTRATEGIA: Calculo aritmetico 
+;; IMPACTO: No destructiva
+;; ========================================================
+
+(defun porcentaje(tiempo-color total-ciclo) ; Total del ciclo rojo 90 + amarillo 6 + verde 120 = 216 seg
+    (* (/ tiempo-color (float total-ciclo)) 100)
+)
+
+;; ========================================================
+;; FUNCIÓN: Informe distribucion temporal
+;; NATURALEZA: Pura (usamos format nil)
+;; ESTRATEGIA: Composicion funcional
+;; IMPACTO: No destructiva
+;; ========================================================
+(defun informe-distribucion-temporal()
+    (list 'rojo (format nil "~,2f%" (porcentaje 90 (duracion-ciclo))) ;; reutilizamos la funcion duracion-ciclo
+          'amarillo (format nil "~,2f%" (porcentaje 6 (duracion-ciclo))) ;; con ~,2f para formatear y quede con 2 dicimales
+          'verde (format nil "~,2f%" (porcentaje 120 (duracion-ciclo)))
+    )
+)
