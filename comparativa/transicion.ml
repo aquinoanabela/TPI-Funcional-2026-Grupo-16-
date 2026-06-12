9097d9a8902f580e2c7eeef87f00eb88d83376fe
Nayuu-vv <estefany070208@gmail.com>
2026-06-12 12:06:20 -0300

    funcion en ocaml transicion

--- comparativa/transicion.ml
+++ comparativa/transicion.ml
@@ -1,23 +1,19 @@
-
 (*en ocaml let tiene la misma utilidad que defun*)
-let transicion (color_actual cambiar-a)
+let transicion color_actual cambiar_a =
   (*let ... in es la variante de let para la declaracion de variables locales*)
-  let colores-incluidos = [en_rojo; en_amarillo; en_verde] in
-  let colores-cambio = [rojo; amarillo; verde] in
+  let colores_incluidos = ["en_rojo"; "en_amarillo"; "en_verde"] in
 
   (*list.mem funciona como member, buscando en la lista el valor que se desea encontrar*)
-  if list.mem color_actual colores-incluidos then
-    None 
-  else 
+  if List.mem color_actual colores_incluidos then
     (*equivalencia de cond en ocaml*)
-    match (color_actual, cambiar-a) with
+    match (color_actual, cambiar_a) with
       (*Some se utiliza para entregar un valor, en este caso some esta asignando a 
       color actual el valor correspondiente*)
-    | (en_verde, amarillo) => Some (color_actual, "cambiar-a-amarillo")
-    | (en_amarillo, rojo) => Some (color_actual, "cambiar-a-rojo")
-    | (en_rojo, verde) => Some (color_actual, "cambiar-a-verde")
+    | ("en_verde", "amarillo") -> Some (color_actual, "cambiar-a-amarillo")
+    | ("en_amarillo", "rojo") -> Some (color_actual, "cambiar-a-rojo")
+    | ("en_rojo", "verde") -> Some (color_actual, "cambiar-a-verde")
       (*ultimo paso del cond, si no es ninguno de los anteriores cambia por defecto*)
-    | _ => Some (color_actual, "cambio-por-defecto")
+    | _ -> Some (color_actual, "cambio-por-defecto")
+  else 
+    None
 ;; 
-   
-   
