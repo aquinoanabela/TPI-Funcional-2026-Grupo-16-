0d7d0780c6ee8d99b1966ca2b4fa446ce48dc28f
Nayuu-vv <estefany070208@gmail.com>
2026-06-14 14:42:45 -0300

    requerimientos 1 y 2 en ocaml

--- comparativa/solucion.ml
+++ comparativa/solucion.ml
@@ -25,3 +25,28 @@ let transicion color_actual cambiar_a =
 ;; 
 
 
+(* ======================================== 
+   REQUERIMIENTO 2: Temporizador automatico
+   NATURALEZA: Pura 
+   ESTRATEGIA: Pattern Matching con Cláusulas de Guarda (when)
+   IMPACTO EN MEMORIA: No destructiva (Retorna un String nuevo) 
+   ======================================= *)
+let timer_con_intermitencia tiempo_unix = 
+ (*en caso de ser un numero negativo*)
+  if tiempo_unix < 0 then 
+    "error-tiempo-invalido"
+else 
+  (*resto de la division, variable local*)
+  let residuo = tiempo_unix mod 225 in
+
+  (*aqui, a r se le asigna el valor del residuo, y luego se compara con los valores*)
+  match residuo with
+  | r when r < 90 -> "en-rojo"
+  | r when r < 93 -> "rojo-intermitente"
+  | r when r < 213 -> "en-verde"
+  | r when r < 216 -> "verde-intermitente"
+  | r when r < 222 -> "en-amarillo"
+  | _              -> "amarillo-intermitente"
+;; 
+
+   
