d9e9d708ab0a4e54df30e72d9a844f45a3545f5b
Nayuu-vv <estefany070208@gmail.com>
2026-06-16 12:28:28 -0300

    actualizacion transicion con intermitencia

--- comparativa/solucion.ml
+++ comparativa/solucion.ml
@@ -5,9 +5,9 @@
    IMPACTO EN MEMORIA: No destructiva (Retorna una estructura Option nueva) 
    ======================================== *)
    (*en ocaml let tiene la misma utilidad que defun*)
-let transicion color_actual cambiar_a =
+let transicion_con_intermitencia color_actual cambiar_a =
   (*let ... in es la variante de let para la declaracion de variables locales*)
-  let colores_incluidos = ["en_rojo"; "en_amarillo"; "en_verde"] in
+  let colores_incluidos = ["en_rojo"; "en_amarillo"; "en_verde"; "rojo_intermitente"; "amarillo_intermitente"; "verde_intermitente"] in
 
   (*list.mem funciona como member, buscando en la lista el valor que se desea encontrar*)
   if List.mem color_actual colores_incluidos then
@@ -15,15 +15,16 @@ let transicion color_actual cambiar_a =
     match (color_actual, cambiar_a) with
       (*Some se utiliza para entregar un valor, en este caso some esta asignando a 
       color actual el valor correspondiente*)
-    | ("en_verde", "amarillo") -> Some (color_actual, "cambiar-a-amarillo")
-    | ("en_amarillo", "rojo") -> Some (color_actual, "cambiar-a-rojo")
-    | ("en_rojo", "verde") -> Some (color_actual, "cambiar-a-verde")
+    | ("en_verde", "verde_intermitente") -> Some (color_actual, "cambiar-a-verde-intermitente")
+    | ("verde_intermitente", "amarillo") -> Some (color_actual, "cambiar-a-amarillo")
+    | ("en_amarillo", "amarillo_intermitente") -> Some (color_actual, "cambiar-a-amarillo-intermitente")
+    | ("amarillo_intermitente", "rojo") -> Some (color_actual, "cambiar-a-rojo")
+    | ("en_rojo", "rojo_intermitente") -> Some (color_actual, "cambiar-a-rojo-intermitente")
       (*ultimo paso del cond, si no es ninguno de los anteriores cambia por defecto*)
     | _ -> Some (color_actual, "cambio-por-defecto")
   else 
     None
 ;; 
-
 
 (* ======================================== 
    REQUERIMIENTO 2: Temporizador automatico
